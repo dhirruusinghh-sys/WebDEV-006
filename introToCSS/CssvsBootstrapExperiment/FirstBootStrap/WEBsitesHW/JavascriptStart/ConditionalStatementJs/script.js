6b55e88958b27513cd238f7f628e723bcc550d62
Your Name <yourgithubemail@gmail.com>
2026-05-01 20:04:59 +0530

    first commit

---
+++ introToCSS/CssvsBootstrapExperiment/FirstBootStrap/WEBsitesHW/JavascriptStart/ConditionalStatementJs/script.js
@@ -0,0 +1,39 @@
+// let a = 10, b = 30;
+
+// function sum(a ,b){
+//     let c = a+b;
+//     return c;
+// }
+// let result = sum (a , b);
+// console.log(result);
+
+// let abc = function () {
+//     console.log("Hello World");
+// };
+// console.log(abc);
+// abc();
+
+// const abc = function () {
+//     console.log ("Hello DheeraJ");
+// };
+// abc();
+
+// let  sub= function(a, b) {
+//     return a - b;
+// };
+
+// console.log(sub);
+// console.log(sub(14,10));
+// console.log(sub());
+
+
+// arrowfunction
+
+const arrowfun =()=> {
+    console.log("I am Singh Dheeraj");
+};
+arrowfun();
+
+
+const addarrow=(a ,b) =>  a + b ; 
+console.log(addarrow(5, 8));
---
+++ introToCSS/CssvsBootstrapExperiment/FirstBootStrap/WEBsitesHW/JavascriptStart/new.js
@@ -0,0 +1,26 @@
+for(var i = 0 ; i<=10; i++)
+{
+    console.log(i);
+}
+
+let a= [24,58,33,86,96,12,53,23,32,100];
+console.log("Array element:");
+for(var i=0; i<10; i++)
+{
+    console.log(a[i]);
+}
+
+
+let i = 11;
+while(i<10)
+{
+    console.log(i);
+    i++;
+}
+
+let k = 1;
+do {
+    console.log("2 x " + k + " = " + (2*k));
+    k++;
+    
+} while (k<=10);
