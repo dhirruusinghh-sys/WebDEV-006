d74cc98de869c338440d72aaf58fb266a854f4ad
Your Name <yourgithubemail@gmail.com>
2026-05-06 00:27:13 +0530

    Add HTML, CSS, and JavaScript files for JavaScript DOM manipulation assignment

---
+++ basichtml/ASSIGNMENTS/script.js
@@ -0,0 +1,29 @@
+function q1Func() {
+        document.getElementById("q1").innerHTML = "Text Changed!";
+    }
+
+    function q2Func() {
+        document.getElementById("q2").innerHTML = "Paragraph Changed!";
+    }
+
+    function q3Func() {
+        document.getElementById("q3").innerHTML = "Welcome";
+    }
+
+    function q4Func() {
+        let text = document.getElementById("q4").innerHTML;
+        console.log(text);
+    }
+
+    function q5Func() {
+        document.getElementById("q5").innerHTML = "New Content using DOM";
+    }
+
+    function q6Func() {
+        document.getElementById("q6").innerHTML = "Changed!";
+    }
+
+    function q7Func() {
+        document.getElementById("q7h").innerHTML = "New Heading";
+        document.getElementById("q7p").innerHTML = "New Paragraph";
+    }
