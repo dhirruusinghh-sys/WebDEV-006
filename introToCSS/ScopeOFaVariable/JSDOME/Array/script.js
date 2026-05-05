86ac1f5a88f144cb5cddd497d7f8f3bf5926a57a
Your Name <yourgithubemail@gmail.com>
2026-05-05 23:07:57 +0530

    Add HTML, CSS, and JavaScript files for array manipulation demonstration

---
+++ introToCSS/ScopeOFaVariable/JSDOME/Array/script.js
@@ -0,0 +1,56 @@
+let arr = [12,23,34,45,56,67,78,89,90];
+
+for(var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+console.log(arr);
+
+arr [2] = arr [2] + 10;
+console.log(arr);
+
+arr = [];
+console.log(arr);
+
+
+ 
+    let arr = [12,23,34,45,56,67,78,89,90,];
+    console.log(arr);
+
+    arr.push(99);
+    console.log(arr);
+
+    arr.pop();
+    console.log(arr);
+
+    arr.shift();
+    console.log(arr);
+
+    arr.unshift(21);
+    console.log(arr);
+
+
+    console.log(arr.at(2));
+
+    console.log(arr.indexOf(56));
+
+    console.log(arr.includes(56));
+    console.log(arr.indexOf(56) !== -1);
+
+let unar = [56,23,89,45,67,34,12,90,87,45,98];
+console.log(unar);
+
+unar.sort();
+console.log(unar);
+
+
+let array = [11,12,13,14,15];
+
+for (var i = 0 ; i < 5; i++){
+    console.log(array[i]);
+}
+
+array.forEach((Element)=>{
+    console.log(Element);});
+
+    
