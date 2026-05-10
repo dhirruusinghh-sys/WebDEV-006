d1277a7058036f80fac85608ff066013dc540a69
Dheeraj Singh <dhirruusinghh@gmail.com>
2026-05-10 10:44:38 +0530

    Refactor Age Calculator form validation and update age display logic

--- basichtml/Projects_New/script.js
+++ basichtml/Projects_New/script.js
@@ -4,13 +4,16 @@ document.querySelector ("form").addEventListener("submit",(event) => {
     const dob = document.getElementById("DOB").value;
     const curDate = document.getElementById("CurrentDate").value;
 
+    if (!dob || !curDate){
+        alert("Please select both dates.");
+        return;
+    }
+
     const Age = Number(curDate.split("-")[0]) - Number (dob.split("-")[0]);
     console.log(Age);
+    document.getElementById("MyAge").innerText = Age;
 
     document.getElementById("DOB").value = "";
     document.getElementById("CurrentDate").value = "";
 
-    document.getElementById("MyAge").innerText ="";
-
-    
 })
