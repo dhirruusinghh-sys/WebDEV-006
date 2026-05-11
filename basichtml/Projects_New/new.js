1253dfa736f1aa3ed658dfecc0fff6c741d0543b
Dheeraj Singh <dhirruusinghh@gmail.com>
2026-05-11 17:27:38 +0530

    Make Add calculator feature with HTML, CSS, and JavaScript implementation

---
+++ basichtml/Projects_New/new.js
@@ -0,0 +1,29 @@
+var display = document.getElementById("display");
+
+function add(value) {
+    if (display.value == "0") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    display.value = "0";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+
+    if (display.value == "") {
+        display.value = "0";
+    }
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
