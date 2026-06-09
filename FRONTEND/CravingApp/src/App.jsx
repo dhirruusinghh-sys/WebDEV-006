d8c03d837f45f2526e47342b8d09d7382abf046e
Dheeraj Singh <dhirruusinghh@gmail.com>
2026-06-09 22:20:44 +0530

    feat: add react-icons dependency; enhance Header and App components with new layout and styling; update main entry point to include Bootstrap JS

--- FRONTEND/CravingApp/src/App.jsx
+++ FRONTEND/CravingApp/src/App.jsx
@@ -1,21 +1,22 @@
 const App = () => {
   return (
-    <div>
-      <div>
-        <h1>My App</h1>
-        <p>Welcome to my app!</p>
-      </div>
+    <>
+      <div className="bg-[url('/commonBG.avif')] bg-cover h-screen w-full flex justify-center items-center">
+        <div className="w-2xs mx-auto border p-20 bg-white/70 rounded-2xl flex flex-col justify-center items-center">
+          <p className="text-green-700">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            culpa consequatur pariatur fuga tempora quam facilis, laboriosam
+            corporis facere consequuntur vero aperiam aspernatur doloribus
+            magnam nam blanditiis commodi ab! Rerum!
+          </p>
 
-      <div className="col-md-4">
-        <div className="card shadow">
-          <div className="card-body">
-            <h4>AI Projects</h4>
-            <p>Practice with real-world projects.</p>
-          </div>
+          <button className="bg-blue-500 px-5 py-2 mt-3 mx-5 rounded text-white hover:bg-blue-700 active:bg-amber-500">
+            submit
+          </button>
         </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
--- FRONTEND/CravingApp/src/Components/Header.jsx
+++ FRONTEND/CravingApp/src/Components/Header.jsx
@@ -1,9 +1,27 @@
-const Header = () => {
-    return (
-        <header className="bg-blue-500 text-white p-4">
-            <h1>My App</h1>
-        </header>
-    )
-}
+import React from "react";
 
-export default Header
+const Header = () => {
+  return (
+    <header className="bg-[#c54a0b] h-28 flex items-center justify-between px-24">
+      
+      <div>
+        <h1 className="text-white text-5xl font-extrabold">
+          crav<span className="text-green-400">i</span>ng
+        </h1>
+      </div>
+
+    
+      <div className="flex items-center gap-8">
+        <button className="text-white text-2xl font-medium hover:text-gray-200 transition">
+          Login
+        </button>
+
+        <button className="bg-white text-[#c54a0b] px-7 py-3 rounded-md text-2xl font-medium hover:bg-gray-100 transition">
+          Register
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
