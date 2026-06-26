6eee2bc3e35b3b4573a5be597a0235c81ab3420e
Dheeraj Singh <dhirruusinghh@gmail.com>
2026-06-26 20:15:53 +0530

    update the email and password correction

--- BACKEND/FirstBackend/src/contollers/auth.controller.js
+++ BACKEND/FirstBackend/src/contollers/auth.controller.js
@@ -1,50 +1,85 @@
 import User from "../models/user.model.js";
 
-export const LoginUser =  (req, res) => {
-  res.json({ message: "login succcessfull" });
+// Login User
+export const LoginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      const error = new Error("Email and Password are required");
+      error.status = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      const error = new Error("User not found");
+      error.status = 404;
+      return next(error);
+    }
+
+    if (password !== existingUser.password) {
+      const error = new Error("Invalid Password");
+      error.status = 401;
+      return next(error);
+    }
+
+    res.status(200).json({
+      message: "Welcome Back",
+      data: existingUser,
+    });
+
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const RegisterUser = async (req, res) => {
+// Register User
+export const RegisterUser = async (req, res, next) => {
   try {
 
-    console.log(req);
-    
     const { fullName, email, password, phone, gender, dob } = req.body;
 
     if (!fullName || !email || !password || !phone || !gender || !dob) {
-      res.status(400).json({ message: "all feild required" });
-      return;
+      return res.status(400).json({
+        message: "All fields are required",
+      });
     }
 
-    const existingUser = await User.findOne({email})
+    const existingUser = await User.findOne({ email });
 
-    if(existingUser){
-        res.status(409).json({message:"Email already registered"})
-        return;
+    if (existingUser) {
+      return res.status(409).json({
+        message: "Email already registered",
+      });
     }
 
-    const photourl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`
+    const photoUrl = `https://placehold.co/600x400?text=${fullName
+      .charAt(0)
+      .toUpperCase()}`;
 
     const photo = {
-        url:photourl,
-        publicId:null
-    }
+      url: photoUrl,
+      publicId: null,
+    };
 
     const newUser = await User.create({
-        fullName,
-        email,
-        gender,
-        photo,
-        password,
-        dob
-    })
+      fullName,
+      email,
+      password,
+      phone,
+      gender,
+      dob,
+      photo,
+    });
 
     res.status(201).json({
-        message:"User Created Sucessfully"
-    })
+      message: "User Created Successfully",
+      data: newUser,
+    });
 
   } catch (error) {
-    console.log(error);
-    
+    next(error);
   }
 };
